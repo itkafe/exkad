eedc55d7bf9c1e84fb9939dd80d44746ff5b5355
itkafe <itkafe@yandex.ru>
2017-10-30 10:44:09 +0400

    Delete test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Simple thing to watch your project and run tests on change
-# Takes one optional argument, which will grep tests and run the ones that match
-
-while inotifywait -r -e modify ./test ./lib ./config; do
-  mix test --trace /home/chris/workspace/exkad/test/exkad_test.exs
-done
